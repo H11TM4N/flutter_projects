0fbaeb6c4ec35ec84f96269f6a28ce98aefc9485
JER3MIAH <jeremiahbrownie@gmail.com>
2023-08-01 08:44:00 +0100

    learning flutter 7

--- learning_flutter/lib/currency_converter.dart
+++ learning_flutter/lib/currency_converter.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 3.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.circular(5),
+    );
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
                 fontSize: 35,
@@ -25,25 +28,21 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             TextField(
-              style: TextStyle(
+              style: const TextStyle(
                 color: Colors.black,
               ),
               decoration: InputDecoration(
                 hintText: 'Please enter the amount in USD',
-                hintStyle: TextStyle(color: Colors.black),
-                prefixIcon: Icon(Icons.monetization_on),
+                hintStyle: const TextStyle(color: Colors.black),
+                prefixIcon: const Icon(Icons.monetization_on),
                 prefixIconColor: Colors.black,
                 filled: true,
                 fillColor: Colors.white,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.red,
-                    width: 2.0,
-                    style: BorderStyle.solid,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(60.0)),
-                ),
+                focusedBorder: border,
+                enabledBorder: border,
               ),
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
             ),
           ],
         ),
