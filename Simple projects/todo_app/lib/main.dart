262677ab44a98e135e50bc5d68c1c9f5487c1b7e
JER3MIAH <jeremiahbrownie@gmail.com>
2023-08-06 22:01:16 +0100

    the todo app scaffold

---
+++ Simple projects/todo_app/lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TodoApp());
+}
+
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'A simple todo app',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('nothing'),
+      ),
+      body: const Column(
+
+      ),
+    );
+  }
+}
