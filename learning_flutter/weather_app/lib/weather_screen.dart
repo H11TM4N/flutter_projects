2a2b5dc0f1e31782e1c53fe1bfcdc815b9188c39
JER3MIAH <jeremiahbrownie@gmail.com>
2023-08-27 12:19:36 +0100

    single child scroll view

--- learning_flutter/weather_app/lib/weather_screen.dart
+++ learning_flutter/weather_app/lib/weather_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class WeatherScreen extends StatelessWidget {
@@ -19,28 +21,189 @@ class WeatherScreen extends StatelessWidget {
           )
         ],
       ),
-      body: const Column(
-        children: [
-          // main card
-          SizedBox(
-            width: double.infinity,
-            child: Card(
-              child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // main card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300°F',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 16),
+                          Icon(Icons.cloud, size: 70),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            'Rain',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            // weather forecast cards
+            const Text(
+              'Weather Forecast',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 12),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
                 children: [
-                  Text('300°F', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),)
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      child: Container(
+                        width: 100,
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),),
+                        child: const Column(
+                          children: [
+                            Text(
+                              '03:00',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 8),
+                            Icon(Icons.cloud, size: 32),
+                            SizedBox(height: 8),
+                            Text('301.12'),
+                      
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      child: Container(
+                        width: 100,
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),),
+                        child: const Column(
+                          children: [
+                            Text(
+                              '03:00',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 8),
+                            Icon(Icons.cloud, size: 32),
+                            SizedBox(height: 8),
+                            Text('301.12'),
+                      
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      child: Container(
+                        width: 100,
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),),
+                        child: const Column(
+                          children: [
+                            Text(
+                              '03:00',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 8),
+                            Icon(Icons.cloud, size: 32),
+                            SizedBox(height: 8),
+                            Text('301.12'),
+                      
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      child: Container(
+                        width: 100,
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),),
+                        child: const Column(
+                          children: [
+                            Text(
+                              '03:00',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 8),
+                            Icon(Icons.cloud, size: 32),
+                            SizedBox(height: 8),
+                            Text('301.12'),
+                      
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    child: Card(
+                      child: Container(
+                        width: 100,
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(12),),
+                        child: const Column(
+                          children: [
+                            Text(
+                              '03:00',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 8),
+                            Icon(Icons.cloud, size: 32),
+                            SizedBox(height: 8),
+                            Text('301.12'),
+                      
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          // weather forecast cards
-          Placeholder(
-            fallbackHeight: 150,
-          ),
-          SizedBox(height: 20),
-          // additional info
-          Placeholder(fallbackHeight: 150)
-        ],
+            const Placeholder(
+              fallbackHeight: 150,
+            ),
+            const SizedBox(height: 20),
+            // additional info
+            const Placeholder(fallbackHeight: 150)
+          ],
+        ),
       ),
     );
   }
