25e52d10f9b1b44331c707a95f42f320895b93f3
JER3MIAH <jeremiahbrownie@gmail.com>
2023-08-27 10:24:43 +0100

    UI update

---
+++ learning_flutter/weather_app/lib/weather_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Weather App",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh),
+          )
+        ],
+      ),
+      body: const Column(
+        children: [
+          // main card
+          SizedBox(
+            width: double.infinity,
+            child: Card(
+              child: Column(
+                children: [
+                  Text('300°F', style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),)
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          // weather forecast cards
+          Placeholder(
+            fallbackHeight: 150,
+          ),
+          SizedBox(height: 20),
+          // additional info
+          Placeholder(fallbackHeight: 150)
+        ],
+      ),
+    );
+  }
+}
