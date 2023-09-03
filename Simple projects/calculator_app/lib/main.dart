9e18dc4a0ef429b6d13ae614839d7d5422ed60e6
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-03 18:57:00 +0100

    fresh start on the calculator app

--- Simple projects/calculator_app/lib/main.dart
+++ Simple projects/calculator_app/lib/main.dart
@@ -8,6 +8,7 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: const CalculatorScreen(),
       theme: ThemeData.dark(useMaterial3: true),
     );
@@ -19,77 +20,21 @@ class CalculatorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calculator'),
-      ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end, // Pushes buttons to the bottom
-          children: <Widget>[
-            // Display the calculator input and output here
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                '0',
-                style: TextStyle(fontSize: 36),
-              ),
-            ),
-            // Create rows of calculator buttons using widgets
-            // Each button should be a separate widget
-            CalculatorButtonRow(['AC', '(', ')', '%']),
-            CalculatorButtonRow(['7', '8', '9', '/']),
-            CalculatorButtonRow(['4', '5', '6', 'x']),
-            CalculatorButtonRow(['1', '2', '3', '-']),
-            CalculatorButtonRow(['0', '.', '=', '+']),
-          ],
-        ),
-      ),
-      // Buttons at the bottom
-    );
-  }
-}
-
-class CalculatorButtonRow extends StatelessWidget {
-  final List<String> buttons;
-
-  const CalculatorButtonRow(this.buttons, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: buttons.map((buttonText) {
-          return Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: CalculatorButton(buttonText),
-            ),
-          );
-        }).toList(),
-      ),
-    );
+    return const Scaffold();
   }
 }
 
 class CalculatorButton extends StatelessWidget {
   final String text;
-
-  const CalculatorButton(this.text, {super.key});
+  const CalculatorButton({super.key, required this.text});
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ElevatedButton(
-        onPressed: () {
-          // Handle button click here
-        },
-        style: ElevatedButton.styleFrom(padding: const EdgeInsets.all(23), textStyle: const TextStyle(fontSize: 23)),
-        child: Text(text),
-      ),
+    return ElevatedButton(
+      onPressed: () {
+        // onpressed action
+      },
+      child: Text(text),
     );
   }
 }
