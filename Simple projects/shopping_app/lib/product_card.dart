890ac3af9e55042a49e7e08c8d1e6e6248bf5c32
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-06 21:28:37 +0100

    Halfway done with the UI

--- Simple projects/shopping_app/lib/product_card.dart
+++ Simple projects/shopping_app/lib/product_card.dart
@@ -4,24 +4,32 @@ class ProductCard extends StatelessWidget {
   final String title;
   final double price;
   final String image;
+  final Color backgroundColor;
   const ProductCard({
     super.key,
     required this.title,
     required this.price,
-    required this.image
+    required this.image,
+    required this.backgroundColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color.fromRGBO(216, 240, 253, 1),
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(title),
+          Text(title, style: Theme.of(context).textTheme.titleMedium),
           const SizedBox(height: 5),
-          Text('\$$price'),
+          Text('\$$price', style: Theme.of(context).textTheme.bodySmall),
           const SizedBox(height: 5),
-          Image.asset(image, height: 175)
+          Center(child: Image.asset(image, height: 175))
         ],
       ),
     );
