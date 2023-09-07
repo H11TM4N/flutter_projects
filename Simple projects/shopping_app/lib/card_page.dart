d650c3bcf868dc3731c7a0c2bd726f95e5e4fda0
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-07 12:02:36 +0100

    Adding ListTile in Cart page

--- Simple projects/shopping_app/lib/card_page.dart
+++ Simple projects/shopping_app/lib/card_page.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 
+import 'global_variables.dart';
+
 class CardPage extends StatelessWidget {
   const CardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Card Page'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Cart'),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: cart.length,
+          itemBuilder: (context, index) {
+            final cartItem = cart[index];
+
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: AssetImage(cartItem['imageUrl'] as String),
+                radius: 30,
+              ),
+              trailing: const Icon(Icons.delete),
+              title: Text(
+                cartItem['title'].toString(),
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              subtitle: Text('Size: ${cartItem['size']}'),
+            );
+          },
+        ));
   }
 }
