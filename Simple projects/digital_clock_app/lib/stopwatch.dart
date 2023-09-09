4e86a3e4ade46d55946c0aebd936a6ffa7fd7982
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-09 17:46:15 +0100

    created a file for stopwatch

---
+++ Simple projects/digital_clock_app/lib/stopwatch.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StopwachScreen extends StatelessWidget {
+  const StopwachScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
