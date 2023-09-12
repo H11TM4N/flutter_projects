e190ff4ee425a3c967bff2052d07bfd670eae32f
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-12 21:02:46 +0100

    edited alarm page

--- Simple projects/digital_clock_app/lib/pages/timer_page.dart
+++ Simple projects/digital_clock_app/lib/pages/timer_page.dart
@@ -5,6 +5,10 @@ class TimerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Timer'),
+      ),
+    );
   }
 }
