ee32d8286c6c2103969dddb1642aa03c701ff3bf
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-20 09:28:11 +0100

    Update

--- Simple projects/expense_tracker/lib/main.dart
+++ Simple projects/expense_tracker/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,16 +7,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const Expenses(),
+    return const MaterialApp(
+      title: 'Expense Tracker App',
+      home: Expenses(),
     );
   }
 }
---
+++ Simple projects/expense_tracker/lib/widgets/expenses.dart
@@ -0,0 +1,42 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:flutter/material.dart';
+
+import '../models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'FLutter Dev',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Python Dev',
+      amount: 15.55,
+      date: DateTime.now(),
+      category: Category.travel,
+    )
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
