a3d4248e5ba69303adfe5d42541715602deb7c2a
JER3MIAH <jeremiahbrownie@gmail.com>
2023-09-27 18:18:04 +0100

    update

---
+++ learning_flutter/practice/lib/Lists/list_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListViewPractice extends StatelessWidget {
+  const ListViewPractice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
