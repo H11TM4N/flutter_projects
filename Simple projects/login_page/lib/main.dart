859e853fb02253c7a2dff304a413a71f7b579d07
JER3MIAH <jeremiahbrownie@gmail.com>
2023-10-03 17:45:22 +0100

    refactoring

--- Simple projects/login_page/lib/main.dart
+++ Simple projects/login_page/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:login_page/login_page.dart';
+import 'package:login_page/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:login_page/pages/login_successful_page.dart';
 import 'firebase_options.dart';
--- Simple projects/login_page/lib/pages/login_page.dart
+++ Simple projects/login_page/lib/pages/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:login_page/register_page.dart';
+import 'package:login_page/pages/register_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
