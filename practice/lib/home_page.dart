7e10e9462857a03b9342871d0176ac1af7a96c44
JER3MIAH <jeremiahbrownie@gmail.com>
2024-01-13 23:01:00 +0100

    Clear app

--- practice/lib/home_page.dart
+++ practice/lib/home_page.dart
@@ -1,64 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:practice/providers/list_tile_provider.dart';
-// import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    ListTile listTile(String title, String route) => ListTile(
-          leading: const Icon(Icons.numbers),
-          title: Text(title),
-          onTap: () => Navigator.pushNamed(context, route),
-        );
-    // ListTileProvider listTileProvider = Provider.of<ListTileProvider>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FLutter Practice'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ExpansionTile(
-              title: const Text('Animations'),
-              leading: const Icon(Icons.animation_sharp),
-              children: [
-                listTile('Animation Containers', 'page1'),
-                listTile('Button Conversion', 'page2'),
-                listTile('Tween Animation', 'page3'),
-                listTile('Login Page Animation', 'page4'),
-                listTile('List Animation', 'page5'),
-                listTile('Progress Animation', 'page6'),
-                listTile('Page Navigation Animation', 'page7'),
-                listTile('Bouncing Ball Animation', 'page8'),
-              ],
-            ),
-            ExpansionTile(
-              title: const Text('API Practice'),
-              leading: const Icon(Icons.api),
-              children: [
-                listTile('Photos API', 'page11'),
-              ],
-            ),
-            ExpansionTile(
-              title: const Text('Bloc'),
-              leading: const Icon(Icons.block),
-              children: [
-                listTile('Login Screen', 'page21'),
-              ],
-            ),
-            ExpansionTile(
-              title: const Text('Zero to mastery'),
-              leading: const Icon(Icons.block),
-              children: [
-                listTile('Riverpod Practice', 'page31'),
-                listTile('TodoApp', 'page41'),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
--- practice/lib/main.dart
+++ practice/lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:practice/API/photos_apii/photos_api.dart';
-import 'package:practice/Animations/custom_painter_animations/bouncing_ball_animation.dart';
-import 'package:practice/Animations/explicit_animations/list_animation.dart';
-import 'package:practice/Animations/explicit_animations/loading_animation.dart';
-import 'package:practice/Bloc_practice/Login_screen/login_screen.dart';
 import 'package:practice/home_page.dart';
-import 'package:practice/Animations/implicit_animations/animation_practice.dart';
 import 'package:flutter/material.dart';
-import 'package:practice/Animations/implicit_animations/button_conversion.dart';
-import 'package:practice/Animations/implicit_animations/pulsating_circle.dart';
-import 'package:practice/Animations/page_route_builder_animation/splash_animation.dart';
-import 'package:practice/zerotomastery/practice.dart';
-import 'package:practice/zerotomastery/todo_app.dart';
-import 'Animations/explicit_animations/login_animation.dart';
 
 void main() {
   runApp(
@@ -29,25 +17,7 @@ class MainPage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Practice App',
       theme: ThemeData(useMaterial3: true),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const HomePage(),
-        'page1': (context) => const AnimationContainer(),
-        'page2': (context) => const ButtonConversion(),
-        'page3': (context) => const PulsatingCircle(),
-        'page4': (context) => const LoginScreenAnimation(),
-        'page5': (context) => const ListAnimation(),
-        'page6': (context) => const RadialProgressAnimation(
-              progress: 0.65,
-              color: Colors.blue,
-            ),
-        'page7': (context) => const SplashAnimation(),
-        'page8': (context) => const BouncingBallAnimation(),
-        'page11': (context) => const PhotosApi(),
-        'page21': (context) => const LoginScreen(),
-        'page31': (context) => const Practice(),
-        'page41': (context) => const TodoScreen(),
-      },
+      home: const HomePage(),
     );
   }
 }
