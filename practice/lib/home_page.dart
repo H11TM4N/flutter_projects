9b959297b800b628f5bd2e73947ee559cb244a6a
JER3MIAH <jeremiahbrownie@gmail.com>
2024-01-21 11:35:02 +0100

    back to json

--- practice/lib/home_page.dart
+++ practice/lib/home_page.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String pizzaString = '';
+
+  Future readJsonFile() async {
+    String myString = await DefaultAssetBundle.of(context)
+        .loadString('assets/pizza_list.json');
+    setState(() {
+      pizzaString = myString;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    readJsonFile();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Jerry'),
+      ),
+      body: SingleChildScrollView(
+        child: Text(pizzaString),
+      ),
+    );
   }
 }
