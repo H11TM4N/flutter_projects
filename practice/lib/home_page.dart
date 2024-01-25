c31cf85e1dc9f424d1cf8171eb2cd5a779a3b3c5
JER3MIAH <jeremiahbrownie@gmail.com>
2024-01-25 19:58:29 +0100

    Clear code

--- practice/lib/home_page.dart
+++ practice/lib/home_page.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:practice/models/pizza.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,42 +8,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String pizzaString = '';
-  List pizzaList = [];
-
-  @override
-  void initState() {
-    super.initState();
-    loadPizzaList();
-  }
-
-  Future<void> loadPizzaList() async {
-    pizzaString = await DefaultAssetBundle.of(context)
-        .loadString('assets/pizza_list.json');
-    setState(() {
-      pizzaList = jsonDecode(pizzaString);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    List<Pizza> myPizzas = [];
-    for (var pizza in pizzaList) {
-      Pizza myPizza = Pizza.fromJson(pizza);
-      myPizzas.add(myPizza);
-    }
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Jerry'),
-      ),
-      body: ListView.builder(
-        itemCount: myPizzas.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(myPizzas[index].pizzaName),
-            subtitle: Text(myPizzas[index].description),
-          );
-        },
+        title: const Text('Jerry'),   
       ),
     );
   }
