46162403fc3b99bf6dfa8abc44786c4d8cbb2906
JER3MIAH <jeremiahbrownie@gmail.com>
2024-02-01 20:57:26 +0100

    useStream in flutter hooks

--- practice/lib/home_page.dart
+++ practice/lib/home_page.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
+    Stream<String> loadTime() {
+      return Stream.periodic(const Duration(seconds: 1), (_) {
+        return DateTime.now().toIso8601String();
+      });
+    }
+
+    final currentTime = useStream(loadTime());
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Jerry'),   
+        title: Text('${currentTime.data}'),
       ),
     );
   }
