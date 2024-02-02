5e599798dfe2a60bd62fc911b3493848a7658094
JER3MIAH <jeremiahbrownie@gmail.com>
2024-02-02 21:24:49 +0100

    flash package in flutter

--- practice/lib/pages/settings_page.dart
+++ practice/lib/pages/settings_page.dart
@@ -1,3 +1,5 @@
+import 'package:flash/flash.dart';
+import 'package:flash/flash_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -7,17 +9,46 @@ class SettingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.go('/setting/$title/dummy');
-          },
-          child: const Text('NFavigate'),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              showFlash(
+                context: context,
+                duration: const Duration(seconds: 3),
+                builder: (context, controller) {
+                  return Toast(
+                    navigatorKey: navigatorKey,
+                    child: const Text('data'),
+                  );
+                },
+              );
+            },
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            child: const Text('Show toast'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
+            child: const Text('Show snackbar'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              context.go('/setting/$title/dummy');
+            },
+            child: const Text('NFavigate'),
+          ),
+        ],
       ),
     );
   }
