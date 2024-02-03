a11c34f9c8bffb132c8c69e0739b52104ea7bf83
JER3MIAH <jeremiahbrownie@gmail.com>
2024-02-03 06:15:18 +0100

    flashbar usage

--- practice/lib/pages/settings_page.dart
+++ practice/lib/pages/settings_page.dart
@@ -20,24 +20,46 @@ class SettingPage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           ElevatedButton(
-            onPressed: () {
-              showFlash(
-                context: context,
-                duration: const Duration(seconds: 3),
-                builder: (context, controller) {
-                  return Toast(
-                    navigatorKey: navigatorKey,
-                    child: const Text('data'),
-                  );
-                },
-              );
-            },
+            onPressed: () {},
             style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
             child: const Text('Show toast'),
           ),
           const SizedBox(height: 20),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              showFlash(
+                context: context,
+                builder: (context, controller) {
+                  return Flash(
+                    controller: controller,
+                    dismissDirections: const [
+                      FlashDismissDirection.startToEnd,
+                      FlashDismissDirection.endToStart
+                    ],
+                    position: FlashPosition.top,
+                    child: FlashBar(
+                      backgroundColor: Colors.blue,
+                      behavior: FlashBehavior.floating,
+                      position: FlashPosition.top,
+                      controller: controller,
+                      content: SizedBox(
+                        height: 70,
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'message',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
             style: ElevatedButton.styleFrom(backgroundColor: Colors.purple),
             child: const Text('Show snackbar'),
           ),
