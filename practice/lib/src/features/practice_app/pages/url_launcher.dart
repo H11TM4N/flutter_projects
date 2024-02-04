0881dd983450906b0742589c73132861f204fdf9
JER3MIAH <jeremiahbrownie@gmail.com>
2024-02-04 22:26:20 +0100

    allow user launch any url

--- practice/lib/src/features/practice_app/pages/url_launcher.dart
+++ practice/lib/src/features/practice_app/pages/url_launcher.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:practice/src/shared/widgets/animated_column.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -9,6 +10,7 @@ class UrlLauncherPage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final callController = useTextEditingController();
+    final urlController = useTextEditingController();
     final buttonStyle = ElevatedButton.styleFrom(
         backgroundColor: Colors.blue,
         minimumSize: Size(MediaQuery.of(context).size.width, 30),
@@ -16,11 +18,24 @@ class UrlLauncherPage extends HookWidget {
           borderRadius: BorderRadius.circular(12),
         ),
         padding: const EdgeInsets.all(17));
+    final textStyle = GoogleFonts.montserrat(
+        color: Colors.white, fontWeight: FontWeight.bold);
 
-    final websiteUrl = useState<Uri>(Uri.parse('https://animepahe.com'));
+    final websiteUrl = useState<Uri>(Uri());
+
+    useEffect(() {
+      urlController.addListener(() {
+        websiteUrl.value = Uri.parse('https://${urlController.text}');
+      });
+      return null;
+    }, [urlController]);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Url Launcher')),
+      appBar: AppBar(
+          title: Text(
+        'Url Launcher',
+        style: GoogleFonts.montserrat(),
+      )),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: AppAnimatedColumn(
@@ -37,12 +52,11 @@ class UrlLauncherPage extends HookWidget {
                 }
               },
               style: buttonStyle,
-              child: const Text('Make phone call'),
+              child: Text('Make phone call', style: textStyle),
             ),
             const SizedBox(height: 50),
-            TextButton(
-              onPressed: () {},
-              child: const Text('https://animepahe.com'),
+            TextField(
+              controller: urlController,
             ),
             const SizedBox(height: 30),
             ElevatedButton(
@@ -53,7 +67,7 @@ class UrlLauncherPage extends HookWidget {
                 );
               },
               style: buttonStyle,
-              child: const Text('Launch in browser'),
+              child: Text('Launch in browser', style: textStyle),
             ),
             const SizedBox(height: 5),
             ElevatedButton(
@@ -64,7 +78,7 @@ class UrlLauncherPage extends HookWidget {
                 );
               },
               style: buttonStyle,
-              child: const Text('Launch in app'),
+              child: Text('Launch in app', style: textStyle),
             ),
           ],
         ),
