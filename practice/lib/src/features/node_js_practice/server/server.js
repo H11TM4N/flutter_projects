0521e17936bc78e0f1105f7120fa135b49198c29
JER3MIAH <jeremiahbrownie@gmail.com>
2024-02-06 17:02:34 +0100

    Apply CORS middleware to allow requests from all origins

--- practice/lib/src/features/node_js_practice/server/server.js
+++ practice/lib/src/features/node_js_practice/server/server.js
@@ -1,19 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors'); // Import the cors middleware
+
+const app = express();
+const port = 3000;
+
+// Apply CORS middleware to allow requests from all origins
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
-})
+    res.send('Hello World!');
+});
 
 const books = [
     { id: 1, title: 'Alice in Wonderland', author: 'Lewis Carrol' },
-    { id: 2, title: 'Around the World in eighty days', author: 'Jules Verne' },
-    { id: 3, title: 'Utopia', author: 'Sir Thomas Moor' },
-]
+    { id: 2, title: 'Around the World in Eighty Days', author: 'Jules Verne' },
+    { id: 3, title: 'Utopia', author: 'Sir Thomas More' },
+];
 
 app.get('/api/books', (req, res) => {
-    res.json(books)
-})
+    res.json(books);
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
