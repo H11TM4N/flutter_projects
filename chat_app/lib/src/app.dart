2d340c3069cd8e1cac8d3b96b732f533e75bbb06
JER3MIAH <jeremiahbrownie@gmail.com>
2024-02-27 10:33:41 +0100

    Set up architecture

---
+++ chat_app/lib/src/app.dart
@@ -0,0 +1,15 @@
+
+import 'package:chat_app/src/shared/shared.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      theme: appTheme,
+    );
+  }
+}
